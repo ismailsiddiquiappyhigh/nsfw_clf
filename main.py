e5001882bc1195c507329ebf75d7eb7ba1552fc1
Mohd Ismail Siddiqui <101191042+ismailsiddiquiappyhigh@users.noreply.github.com>
2024-05-30 16:20:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=1417, workers=1, reload=False)
+    uvicorn.run("api:app", host="0.0.0.0", port=6767, workers=1, reload=False)
